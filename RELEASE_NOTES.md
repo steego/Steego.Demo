be1c91184fea741a18f6722a7c271337eee99e12
Steve Goguen <sgoguen@gmail.com>
2017-09-17 16:24:27 -0400

    Added explorer code

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+### 1.0.2.1
+Added explorer
+
 ### 1.0.2
 * Added web server and html module
 
--- src/Steego.Demo/WebServer.fs
+++ src/Steego.Demo/WebServer.fs
@@ -3,18 +3,23 @@ module Steego.Demo.WebServer
 //  Encapsulate everything in 
 open Suave
 
-let private started = ref false
-let private mainWebPart = ref (Successful.OK "Welcome")
+type private WebInstance() = 
+    let started = ref false
+    let mainWebPart = ref (Successful.OK "Welcome")
 
-let update(newPart) = lock mainWebPart (fun () -> mainWebPart := newPart)
+    member this.Update(newPart) = lock mainWebPart (fun () -> mainWebPart := newPart)
 
-let start() = 
-    if not started.Value then
-        lock started (fun () -> started := true)
-        async {
-                startWebServer defaultConfig (fun(ctx : HttpContext) ->
-                    async {
-                        let part = lock mainWebPart (fun () -> mainWebPart.Value)
-                        return! part ctx
-                    })
-        } |> Async.Start
+    member this.Start() = 
+        if not started.Value then
+            lock started (fun () -> started := true)
+            async {
+                    startWebServer defaultConfig (fun(ctx : HttpContext) ->
+                        async {
+                            let part = lock mainWebPart (fun () -> mainWebPart.Value)
+                            return! part ctx
+                        })
+            } |> Async.Start
+
+let private instance = lazy WebInstance()
+let update(newPart) = instance.Value.Update(newPart)
+let start() = instance.Value.Start()
